e84758c077f40a8328736d776515f3d2665af575
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-01-24 03:25:36 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/sugarmaker/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4 --timeout 120 -p start=0,d=0 -t 6
+/root/sugarmaker/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4.zeop --timeout 120 -p start=0,d=0 -t 6
