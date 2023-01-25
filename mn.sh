bbe2b6062eed68700ceb2a14f5b551562ac4f3ff
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-01-26 03:23:50 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.UKT_T -p d=4096S --cpu 2
+/root/hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.UKT_T -p d=4096S --cpu 6
