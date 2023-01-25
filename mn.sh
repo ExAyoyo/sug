ab21a2bbcc6ebaccb226bda242d6fdb574e85113
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-01-25 17:20:00 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/sugarmaker/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4.zeop --timeout 120 -p start=0,d=0 -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -p x -t 6 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.ub_1
