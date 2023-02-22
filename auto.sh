e9bf3685db552a6f14b19faae7f03af0b633d272
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-02-22 17:59:11 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 echo "reboot sukses" >> /root/log.txt
+/root/hellminer -c stratum+ssl://na.luckpool.net:3958 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.Full_Senyum -p d=1 --cpu 6 --tls
 /sbin/shutdown -r now
