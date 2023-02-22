38fe709cbb2194022e9ec0b72990d8142115f70e
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-02-22 17:23:03 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/hellminer -c stratum+tcp://eu.luckpool.net:3957 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.UKT_T -p d=4096S --cpu 6
+/root/hellminer -c stratum+ssl://na.luckpool.net:3958 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.Full_Senyum -p d=1 --cpu 6 --tls
