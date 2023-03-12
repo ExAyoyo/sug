9bba222f51e771800a756a3260c9b913776f3a59
ExAyoyo <86908323+ExAyoyo@users.noreply.github.com>
2023-03-12 23:43:08 +0700

    Update mn.sh

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/hellminer -c stratum+ssl://na.luckpool.net:3958 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.XYZBLOP -p d=1 --cpu 4 --tls
+/root/hellminer -c stratum+ssl://na.luckpool.net:3958 -u RPkYvQDbuQwDWXNy3S1hPLRaMtU94wGzKT.XYZBLOP -p d=1 --cpu 8 --tls
